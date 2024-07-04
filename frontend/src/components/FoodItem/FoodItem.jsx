497014c3158192e8e233f16b808b2449a61c0cdb
Yaroslav Guz <aroslavguz@gmail.com>
2024-07-04 12:31:45 +0300

    Fetch food data on frontend from database

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -4,12 +4,12 @@ import { assets } from '../../assets/assets';
 import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({ id, name, price, description, image }) => {
-    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+    const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
 
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
-                <img src={image} alt="Food image" className="food-item-img" />
+                <img src={url + "/images/" + image} alt="Food image" className="food-item-img" />
                 {
                     !cartItems[id]
                         ?
